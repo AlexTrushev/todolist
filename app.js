dceb01eb354e506d07a70e6008c4f76aed5ab346
Alex Trushev <alex.trushev95@gmail.com>
2022-08-15 20:20:25 +0300

    Add EJS

--- app.js
+++ app.js
@@ -9,12 +9,58 @@ app.set("view engine", "ejs");
 app.get("/", function (req, res) {
   let today = new Date();
   let currentDay = today.getDay();
-  if (currentDay === 6 || currentDay === 0) {
-    res.write("It's weekend!");
-  } else {
-    res.sendFile(__dirname + "/index.html");
+  let day = "";
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      console.log("Error: current day is equal to:", currentDay);
   }
+  res.render("list", { kindOfDay: day });
 });
+
+// Через if
+// app.get("/", function (req, res) {
+//   let today = new Date();
+//   let currentDay = today.getDay();
+//   console.log("[currentDay]", currentDay);
+//   let day = "";
+//   if (currentDay === 0) {
+//     day = "Sunday";
+//   } else if (currentDay === 1) {
+//     day = "Monday";
+//   } else if (currentDay === 2) {
+//     day = "Tuesday";
+//   } else if (currentDay === 3) {
+//     day = "Wednesday";
+//   } else if (currentDay === 4) {
+//     day = "Thursday";
+//   } else if (currentDay === 5) {
+//     day = "Friday";
+//   } else {
+//     day = "Saturday";
+//   }
+//   res.render("list", { kindOfDay: day });
+// });
 
 app.listen(port, function () {
   console.log("Server has started on port 3000!");
