3705d54de98b171a2cabf97756eb26f0fc6710d1
Alex Trushev <alex.trushev95@gmail.com>
2022-08-24 20:59:52 +0300

    Add page "Work", delete old comments

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { request } = require("express");
 
 const app = express();
 const port = 3000;
 
 let items = ["Buy food", "Cook food", "Eat food"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -20,76 +22,30 @@ app.get("/", function (req, res) {
   };
   let day = today.toLocaleDateString("en-US", options);
   res.render("list", {
-    kindOfDay: day,
+    listTitle: day,
     newListItems: items,
   });
 });
 
 app.post("/", function (req, res) {
   let item = req.body.newItem;
-  items.push(item);
-  console.log("[items]", items);
-  console.log("[itemsLength]", items.length);
-  res.redirect("/");
+  console.log("[req.body.list]", req.body.list);
+  if (req.body.list === "Work List") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems,
+  });
 });
 
 app.listen(port, function () {
   console.log("Server has started on port 3000!");
 });
-
-// Через switch
-// app.get("/", function (req, res) {
-//   let today = new Date();
-//   let currentDay = today.getDay();
-//   let day = "";
-//   switch (currentDay) {
-//     case 0:
-//       day = "Sunday";
-//       break;
-//     case 1:
-//       day = "Monday";
-//       break;
-//     case 2:
-//       day = "Tuesday";
-//       break;
-//     case 3:
-//       day = "Wednesday";
-//       break;
-//     case 4:
-//       day = "Thursday";
-//       break;
-//     case 5:
-//       day = "Friday";
-//       break;
-//     case 6:
-//       day = "Saturday";
-//       break;
-//     default:
-//       console.log("Error: current day is equal to:", currentDay);
-//   }
-//   res.render("list", { kindOfDay: day });
-// });
-
-// Через if
-// app.get("/", function (req, res) {
-//   let today = new Date();
-//   let currentDay = today.getDay();
-//   console.log("[currentDay]", currentDay);
-//   let day = "";
-//   if (currentDay === 0) {
-//     day = "Sunday";
-//   } else if (currentDay === 1) {
-//     day = "Monday";
-//   } else if (currentDay === 2) {
-//     day = "Tuesday";
-//   } else if (currentDay === 3) {
-//     day = "Wednesday";
-//   } else if (currentDay === 4) {
-//     day = "Thursday";
-//   } else if (currentDay === 5) {
-//     day = "Friday";
-//   } else {
-//     day = "Saturday";
-//   }
-//   res.render("list", { kindOfDay: day });
-// });
