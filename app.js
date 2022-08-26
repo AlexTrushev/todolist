b6516f50ea511177e544e4cfba102d3eebd036c3
Alex Trushev <alex.trushev95@gmail.com>
2022-08-26 16:12:22 +0300

    Add include, add module, refactoring

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+//jshint esversion:6
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const { request } = require("express");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 const port = 3000;
 
-let items = ["Buy food", "Cook food", "Eat food"];
-let workItems = [];
+const items = ["Buy food", "Cook food", "Eat food"];
+const workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -14,13 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-  let day = today.toLocaleDateString("en-US", options);
+  const day = date.getDate();
   res.render("list", {
     listTitle: day,
     newListItems: items,
@@ -28,7 +25,7 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
   console.log("[req.body.list]", req.body.list);
   if (req.body.list === "Work List") {
     workItems.push(item);
